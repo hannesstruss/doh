37f263a159da5bd0dde047ec9403e20d2e4932dc
Hannes Struß <x@hannesstruss.de>
2020-08-21 12:32:12 +0200

    Cycle through five dummy images

--- dohkt/grab/src/main/kotlin/doh/grab/FakeImageGrabber.kt
+++ dohkt/grab/src/main/kotlin/doh/grab/FakeImageGrabber.kt
@@ -8,18 +8,22 @@ import java.time.LocalDateTime
 class FakeImageGrabber(
   private val imageDir: File
 ) : ImageGrabber {
+  private var imageIndex = 0
+
   override suspend fun grabImage(): File = withContext(Dispatchers.IO) {
     val timestamp = LocalDateTime.now().toString()
     val targetFile = imageDir.resolve("$timestamp.jpg")
 
     check(!targetFile.exists()) { "You're trying to grab images too fast!" }
 
-    val resourceStream = javaClass.getResourceAsStream("/sourdough-dummy.jpg")
+    val resourceStream = javaClass.getResourceAsStream("/sourdough-dummy-${imageIndex + 1}.jpg")
     resourceStream.use { inStream ->
       targetFile.outputStream().use { outStream ->
         inStream.copyTo(outStream)
       }
     }
+
+    imageIndex = (imageIndex + 1) % 5
 
     targetFile
   }
