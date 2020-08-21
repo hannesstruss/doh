1717770b41f7d3d0b04f9f85043420965337ff4d
Hannes Struß <x@hannesstruss.de>
2020-08-21 10:13:34 +0200

    Remove cast

--- dohkt/build.gradle.kts
+++ dohkt/build.gradle.kts
@@ -36,7 +36,7 @@ tasks.register("deployToPi") {
   doLast {
     exec {
       standardOutput = System.out
-      val distZipProvider = project(":app").tasks.named("distZip") as TaskProvider<Zip>
+      val distZipProvider = project(":app").tasks.named<Zip>("distZip")
       val archivePath = distZipProvider.get().archiveFile.get().asFile.absolutePath
 
       commandLine = listOf("bash", "gradle/deploypi.sh", archivePath)
