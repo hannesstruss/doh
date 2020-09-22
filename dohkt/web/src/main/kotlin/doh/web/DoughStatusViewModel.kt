8b4ddd9d8a6bd68eaf19235ac6f60449447b3bd8
Hannes Struß <x@hannesstruss.de>
2020-09-22 13:12:00 +0200

    Format date/time and display in local zone

--- dohkt/web/src/main/kotlin/doh/web/DoughStatusViewModel.kt
+++ dohkt/web/src/main/kotlin/doh/web/DoughStatusViewModel.kt
@@ -2,6 +2,8 @@ package doh.web
 
 import doh.db.DoughStatus
 import kotlinx.serialization.Serializable
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 @Serializable
 data class DoughStatusViewModel(
@@ -12,9 +14,11 @@ data class DoughStatusViewModel(
   val growth: Double
 ) {
   companion object {
+    private val formatter = DateTimeFormatter.ofPattern("d.LL.uuuu, HH:mm")
+
     fun fromDoughStatus(imagesPath: String, doughStatus: DoughStatus) = DoughStatusViewModel(
       id = doughStatus.id.toString(),
-      recordedAt = doughStatus.recordedAt.toString(),
+      recordedAt = doughStatus.recordedAt.atZone(ZoneId.of("Europe/Berlin")).format(formatter),
       recordedAtEpochSeconds = doughStatus.recordedAt.epochSecond,
       imagePath = "$imagesPath/${doughStatus.imageFile}",
       growth = doughStatus.growth
