19edf227aa2095640425075aa70003b01968d436
Hannes Struß <x@hannesstruss.de>
2020-09-22 11:21:07 +0200

    Fix deploy task

--- dohkt/build.gradle.kts
+++ dohkt/build.gradle.kts
@@ -37,7 +37,7 @@ tasks.register("deployToPi") {
   doLast {
     exec {
       standardOutput = System.out
-      val distZipProvider = project(":doh.doh.app").tasks.named<Zip>("distZip")
+      val distZipProvider = project(":app").tasks.named<Zip>("distZip")
       val archivePath = distZipProvider.get().archiveFile.get().asFile.absolutePath
 
       commandLine = listOf("bash", "gradle/deploypi.sh", archivePath)
