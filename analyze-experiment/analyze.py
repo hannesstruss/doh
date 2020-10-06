9cb8e9d3b772e017cf677f48af24df7bf6d434e0
Hannes Struß <x@hannesstruss.de>
2020-10-06 19:34:31 +0200

    WIP analyze

--- analyze-experiment/analyze.py
+++ analyze-experiment/analyze.py
@@ -8,6 +8,30 @@ from skimage import color
 from skimage.filters import threshold_otsu as threshold
 from skimage.measure import regionprops, label
 
+CROP_X1 = 790
+CROP_X2 = 850
+CROP_Y1 = 190
+CROP_Y2 = 950
+GLASS_BOTTOM_Y = 935 - CROP_Y1
+
+def crop(img):
+    return img[CROP_Y1:CROP_Y2, CROP_X1:CROP_X2]
+
+def find_rubber_band_y(img):
+    hsv_img = color.rgb2hsv(img)
+    hue_img = hsv_img[:, :, 0]
+    sat_img = hsv_img[:, :, 1]
+    binary_img = (hue_img > 0.45) & (hue_img < 0.65) & (sat_img > 0.2)
+
+    regions = regionprops(label(binary_img))
+    best_region_y, score = None, 0.0
+    for props in regions:
+        minr, minc, maxr, maxc = props.bbox
+        width = 
+
+
+    return None, binary_img
+
 if __name__ == "__main__":
     images_dir = "/Users/hannes/Desktop/doh-images/"
     images = os.listdir(images_dir)
@@ -19,9 +43,9 @@ if __name__ == "__main__":
 
     ambient, backlit = paired_images[index]
 
-    backlit_img = io.imread(backlit)
-    ambient_img = io.imread(ambient)
+    backlit_img = crop(io.imread(backlit))
+    ambient_img = crop(io.imread(ambient))
     fig, ax = plt.subplots(1, 2, figsize=(12, 8))
     ax[0].imshow(backlit_img)
-    ax[1].imshow(ambient_img)
+    ax[1].imshow(find_rubber_band_y(ambient_img)[1], cmap="gray")
     plt.show()
