5856fcf31f7ca1be1eae17f02811e19d4b8022a6
Hannes Struß <x@hannesstruss.de>
2021-01-20 17:06:41 +0100

    Add disk usage metric

--- system-metrics/app.go
+++ system-metrics/app.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"bufio"
 	"strings"
 	"strconv"
@@ -54,10 +55,28 @@ func getMemory() (uint64, uint64) {
 	return totalMem, availableMem
 }
 
+func getDiskUsage() (uint64, uint64) {
+	out, err := exec.Command("df", "-P", "/").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	content := string(out)
+
+	secondLineParts := strings.Fields(strings.Split(content, "\n")[1])
+	totalSpace, _ := strconv.ParseUint(secondLineParts[1], 10, 64)
+	usedSpace, _ := strconv.ParseUint(secondLineParts[2], 10, 64)
+
+	return totalSpace, usedSpace
+}
+
 func metrics(w http.ResponseWriter, r *http.Request) {
 	totalMem, availableMem := getMemory()
 	writeMetric(w, "doh32_memory_available_kb", "The used memory on the RPi in kb.", float64(availableMem))
 	writeMetric(w, "doh32_memory_total_kb", "The total available memory on the RPi in kb.", float64(totalMem))
+
+	totalSpace, usedSpace := getDiskUsage()
+	writeMetric(w, "doh32_disk_total_kb", "Total available disk space in kb.", float64(totalSpace))
+	writeMetric(w, "doh32_disk_used_kb", "Total used disk space in kb.", float64(usedSpace))
 }
 
 func main() {
